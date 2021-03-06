cc5ced9087d5b18be8c0fac9b9e8c5a9924c05df
lewisdecarolis <lsd81882@creighton.edu>
2021-03-05 18:28:10 -0600

    for

---
+++ forms/while/while.js
@@ -0,0 +1,13 @@
+/*
+//Declares variables
+let i = []
+let doneWithI = false 
+
+//Shows while statement, determines when loop breaks
+while (doneWithI == false) {
+    console.log(`This is loop iteration ${i}`)
+    i++
+    doneWithI = confirm ("Are you done with the I?")    
+    }
+    console.log(i)
+*/
